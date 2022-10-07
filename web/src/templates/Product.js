c1a93c36704b334e6c87e8fcad135d4d45f631fd
Derrick Snow <74995064+suprawhiz@users.noreply.github.com>
2022-10-07 12:59:31 -0700

    btn change

--- web/src/templates/Product.js
+++ web/src/templates/Product.js
@@ -220,11 +220,11 @@ const Product = ({ data: { item } }) => {
                 <BuyButton
                   className="snipcart-add-item"
                   data-item-id={item.id}
-                  data-item-price={selected.price}
+                  data-item-price={Number(selected.price)}
                   data-item-name={item.title}
                   data-item-description={item.blurb.en}
                   data-item-image={variants[0].images[0].asset.url}
-                  data-item-url={`${siteMetadata.siteUrl}/products/${item.slug.current}`}
+                  // data-item-url={`${siteMetadata.siteUrl}/products/${item.slug.current}`}
                   data-item-custom1-name={
                     variants.length > 1 ? item.variant_type : ""
                   }
