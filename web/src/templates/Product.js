3238b49878edcd0ad9a7e8f70a9fab6368151125
Derrick Snow <74995064+suprawhiz@users.noreply.github.com>
2022-10-07 15:40:06 -0700

    repush of push

--- web/src/templates/Product.js
+++ web/src/templates/Product.js
@@ -225,7 +225,7 @@ const Product = ({ data: { item } }) => {
                   data-item-name={item.title}
                   data-item-description={item.blurb.en}
                   data-item-image={variants[0].images[0].asset.url}
-                  // data-item-url={`${siteMetadata.siteUrl}/products/${item.slug.current}`}
+                  data-item-url={`${siteMetadata.siteUrl}/products/${item.slug.current}`}
                   data-item-custom1-name={
                     variants.length > 1 ? item.variant_type : ""
                   }
