9a8b32c848f856850c2a8e0e46dcb250c40cbb31
Derrick Snow <74995064+suprawhiz@users.noreply.github.com>
2022-10-07 15:11:54 -0700

    hopes

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -31,6 +31,7 @@ module.exports = {
         version: `3.0.29`,
         publicApiKey: `MmQyYWY5YjctZTZhNy00OTY5LWE3N2QtMmViYTUzNDVkNTcxNjM3OTk4MzMxNzQxMTk2NDA4
         `,
+        loadStrategy: "on-user-interaction",
       },
     },
     `gatsby-plugin-image`,
--- web/src/templates/Product.js
+++ web/src/templates/Product.js
@@ -175,7 +175,7 @@ const Product = ({ data: { item } }) => {
 
   const imageData = getGatsbyImageData(
     variants[0].images[0].asset.id,
-    { maxWidth: 600 },
+    { maxWidth: 500 },
     steel.sanity
   );
 
@@ -225,7 +225,7 @@ const Product = ({ data: { item } }) => {
                   data-item-name={item.title}
                   data-item-description={item.blurb.en}
                   data-item-image={variants[0].images[0].asset.url}
-                  data-item-url={`${siteMetadata.siteUrl}/products/${item.slug.current}`}
+                  // data-item-url={`${siteMetadata.siteUrl}/products/${item.slug.current}`}
                   data-item-custom1-name={
                     variants.length > 1 ? item.variant_type : ""
                   }
