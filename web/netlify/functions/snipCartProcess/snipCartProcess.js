c52abec830eed9b579a710506b66b0ed84d7fd7c
Alec Venzor <alecvenzordev@gmail.com>
2022-10-11 19:08:34 -0700

    .

--- web/netlify/functions/snipCartProcess/snipCartProcess.js
+++ web/netlify/functions/snipCartProcess/snipCartProcess.js
@@ -10,22 +10,20 @@ const client = sanityClient({
 exports.handler = async function (event, context) {
   const query = `*[_type == "product"]{_id,slug{current},variants[0]{price}}`;
   const products = await client.fetch(query).then((results) => {
-    console.log(results);
-    const productJson = [];
     const allProducts = results.map((product) => {
       let productDef = {
         id: product.slug.current,
         url: `/.netlify/functions/snipCartProcess`,
         price: product.variants.price,
       };
-      productJson.push(productDef);
+      return productDef;
     });
-    console.log(productJson);
+    console.log(allProducts);
     return allProducts;
   });
   return {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(productJson),
+    body: JSON.stringify(products),
   };
 };
