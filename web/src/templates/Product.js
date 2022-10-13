88793483b1ec11e14c466527e4fa069c274b1dc6
Alec Venzor <alecvenzordev@gmail.com>
2022-10-12 17:21:25 -0700

    .

--- web/src/templates/Product.js
+++ web/src/templates/Product.js
@@ -220,7 +220,7 @@ const Product = ({ data: { item } }) => {
 
                 <BuyButton
                   className="snipcart-add-item"
-                  data-item-id={item.id}
+                  data-item-id={item.slug.current}
                   data-item-price={selected.price}
                   data-item-name={item.slug.current}
                   data-item-description={item.blurb.en}
